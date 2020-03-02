3aeb2792c4b8270f402caf0b69494d046c33148c
CppPhil <CppPhil@users.noreply.github.com>
2020-03-02 20:51:46 +0100

    Adjust format.sh

--- format.sh
+++ format.sh
@@ -7,6 +7,7 @@ readonly DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 # Directories
 readonly SRC_DIR=$DIR/src
+readonly INCLUDE_DIR=$DIR/include
 
 # files
 readonly DOT_CLANG_FORMAT=$DIR/.clang-format
@@ -27,5 +28,6 @@ format () {
 
 # Format the source files.
 format $SRC_DIR
+format $INCLUDE_DIR
 
 exit 0
