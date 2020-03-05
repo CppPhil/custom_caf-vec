2511319c3759fd5c327cd8ce30a8adf2ca5c62ee
CppPhil <CppPhil@users.noreply.github.com>
2020-03-05 10:09:48 +0100

    Adjust log format

--- src/initialize_spdlog.cpp
+++ src/initialize_spdlog.cpp
@@ -20,6 +20,6 @@ void initialize_spdlog() {
     "caf-vec logger", sinks.begin(), sinks.end(), spdlog::thread_pool(),
     spdlog::async_overflow_policy::block);
   spdlog::set_default_logger(logger);
-  spdlog::set_pattern("%^[%d.%m.%Y %T.%e]%$ [%s:%# %!] [tid %t]: %v");
+  spdlog::set_pattern("%^[%d.%m.%Y %T.%e]%$ [%s:%# %!] [thread %t]: \"%v\"");
 }
 } // namespace vec
